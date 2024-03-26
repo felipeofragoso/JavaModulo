0c48fdf14f84b5539a19b9c4382619d6e141dcfc
Felipe Fragoso <58962669+felipeofragoso@users.noreply.github.com>
2024-03-25 21:01:02 -0300

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+const soma = require("./soma")
+
+const subtracao = require("./subtracao")
+const multiplicacao = require("./multiplicacao")
+const div
+
+
+console.log(soma(6,2))
+
+console.log(subtracao(6,2))
+
+console.log(multiplicacao(6,2))
+
+console.log(divisao(6,2))
